4f182e099879ecde4c3ccaee3a2e2c82914b7a69
Ismail-Iyada <ismailiyada@gmail.com>
2023-11-03 00:48:48 +0100

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+//connecting do mongoDb database
+mongoose
+  .connect(process.env.MONGO) //Mongo is the connection string
+  .then(() => {
+    //if connected successfully log it 
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    //else log error
+    console.log(err);
+  });
 
-const app = express()
+const app = express();
 
-app.listen(5000, ()=>{
-    console.log("server is running on port 5000");
-})
-
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
+});
